abe51915f15fef0e1680a0fd1765e4f8e1953bef
joris68 <joris.gabrisch@web.de>
2024-03-29 16:34:15 +0100

    added black pipeline and formatted code

--- src/Main.py
+++ src/Main.py
@@ -5,79 +5,85 @@ from pybeamline.sources import log_source
 import logging
 
 
-
 def setup_loggers(path_detailed, path_output, type):
-    high_level_logger = logging.getLogger('high_level_' + type)
-    detailed_logger = logging.getLogger('detailed_' +type)
+    high_level_logger = logging.getLogger("high_level_" + type)
+    detailed_logger = logging.getLogger("detailed_" + type)
 
     high_level_logger.setLevel(logging.INFO)
     detailed_logger.setLevel(logging.DEBUG)
 
-    high_handler = logging.FileHandler(path_output, 'w', 'utf-8')
-    detailed_handler = logging.FileHandler(path_detailed, 'w', 'utf-8')
+    high_handler = logging.FileHandler(path_output, "w", "utf-8")
+    detailed_handler = logging.FileHandler(path_detailed, "w", "utf-8")
 
     # creating a csv logger
-    formatter_detailed = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
-    formatter_output = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s')
-    #high_handler.setFormatter(formatter)
-    #detailed_handler.setFormatter(formatter)
+    formatter_detailed = logging.Formatter("%(asctime)s, %(levelname)s, %(message)s")
+    formatter_output = logging.Formatter("%(asctime)s, %(levelname)s, %(message)s")
+    # high_handler.setFormatter(formatter)
+    # detailed_handler.setFormatter(formatter)
 
     # Add handlers to the logger
     high_level_logger.addHandler(high_handler)
     detailed_logger.addHandler(detailed_handler)
 
 
-
 def sudden():
 
-  logging.basicConfig(filename='logs/logs_sudden.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(
+        filename="logs/logs_sudden.log", encoding="utf-8", level=logging.DEBUG
+    )
 
-  my_trace_map_handler = TraceMapHandler(traceMapSize=40)
+    my_trace_map_handler = TraceMapHandler(traceMapSize=40)
 
-  my_model_handler = ModelHandler(
-      my_trace_map_handler,
-      allowed_deviation=1.7,
-      trace_Treshold=0.6,
-  )
+    my_model_handler = ModelHandler(
+        my_trace_map_handler,
+        allowed_deviation=1.7,
+        trace_Treshold=0.6,
+    )
 
-  my_process_history = ProcessHistory(
-      my_model_handler,
-      lower_boundary=200,
-      anomaly_treshhold=0.7,
-      cohens_boundary=12.0,
-      model_epsilon=0.3
-  )
+    my_process_history = ProcessHistory(
+        my_model_handler,
+        lower_boundary=200,
+        anomaly_treshhold=0.7,
+        cohens_boundary=12.0,
+        model_epsilon=0.3,
+    )
 
-  log_source("Data/synth/sudden_time_noise0_100_baseline.xes").pipe(
+    log_source("Data/synth/sudden_time_noise0_100_baseline.xes").pipe().subscribe(
+        lambda x: my_process_history.concept_Drift_detection(x)
+    )
 
-  ).subscribe(lambda x: my_process_history.concept_Drift_detection(x))
 
 def incremental():
 
-  logging.basicConfig(filename='logs/logs_incremental.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(
+        filename="logs/logs_incremental.log", encoding="utf-8", level=logging.DEBUG
+    )
 
-  my_trace_map_handler = TraceMapHandler(traceMapSize=40)
+    my_trace_map_handler = TraceMapHandler(traceMapSize=40)
 
-  my_model_handler = ModelHandler(
-      my_trace_map_handler,
-      allowed_deviation=1.7,
-      trace_Treshold=0.6,
-  )
+    my_model_handler = ModelHandler(
+        my_trace_map_handler,
+        allowed_deviation=1.7,
+        trace_Treshold=0.6,
+    )
 
-  my_process_history = ProcessHistory(
-      my_model_handler,
-      lower_boundary=200,
-      anomaly_treshhold=0.7,
-      cohens_boundary=12.0,
-      model_epsilon=0.3
-  )
+    my_process_history = ProcessHistory(
+        my_model_handler,
+        lower_boundary=200,
+        anomaly_treshhold=0.7,
+        cohens_boundary=12.0,
+        model_epsilon=0.3,
+    )
 
-  log_source("Data/synth/incremental_time_noise0_100_baseline.xes").pipe(
+    log_source("Data/synth/incremental_time_noise0_100_baseline.xes").pipe().subscribe(
+        lambda x: my_process_history.concept_Drift_detection(x)
+    )
 
-  ).subscribe(lambda x: my_process_history.concept_Drift_detection(x))
-   
+
 def recurring():
-    logging.basicConfig(filename='logs/logs_recurring.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(
+        filename="logs/logs_recurring.log", encoding="utf-8", level=logging.DEBUG
+    )
 
     my_trace_map_handler = TraceMapHandler(traceMapSize=40)
 
@@ -92,15 +98,18 @@ def recurring():
         lower_boundary=200,
         anomaly_treshhold=0.7,
         cohens_boundary=12.0,
-        model_epsilon=0.3
+        model_epsilon=0.3,
     )
 
-    log_source("Data/synth/recurring_time_noise0_100_baseline.xes").pipe(
+    log_source("Data/synth/recurring_time_noise0_100_baseline.xes").pipe().subscribe(
+        lambda x: my_process_history.concept_Drift_detection(x)
+    )
 
-    ).subscribe(lambda x: my_process_history.concept_Drift_detection(x))
-   
+
 def gradual():
-    logging.basicConfig(filename='logs/logs_gradual.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(
+        filename="logs/logs_gradual.log", encoding="utf-8", level=logging.DEBUG
+    )
 
     my_trace_map_handler = TraceMapHandler(traceMapSize=40)
 
@@ -115,13 +124,12 @@ def gradual():
         lower_boundary=200,
         anomaly_treshhold=0.7,
         cohens_boundary=12.0,
-        model_epsilon=0.3
+        model_epsilon=0.3,
     )
 
-    log_source("Data/synth/recurring_time_noise0_100_baseline.xes").pipe(
-
-    ).subscribe(lambda x: my_process_history.concept_Drift_detection(x))
-   
+    log_source("Data/synth/recurring_time_noise0_100_baseline.xes").pipe().subscribe(
+        lambda x: my_process_history.concept_Drift_detection(x)
+    )
 
 
 if __name__ == "__main__":
--- src/TimeModel.py
+++ src/TimeModel.py
@@ -1,16 +1,13 @@
-
 class TimeModel:
 
-     # deviation will be in standarddeviations from the average
-     # the structure will be like this:
-     # key will be a relation tuple from the directly follows map
-     # value will be a list of lenght two : [average, std]
-     # unit of both will be (milli)seconds.
+    # deviation will be in standarddeviations from the average
+    # the structure will be like this:
+    # key will be a relation tuple from the directly follows map
+    # value will be a list of lenght two : [average, std]
+    # unit of both will be (milli)seconds.
 
+    def __init__(self, times: dict) -> None:
+        self.times = times
 
-     def __init__(self, times: dict) -> None:
-          self.times = times
-
-     def __str__(self):
-        return '\n'.join([f"{key}: {value}" for key, value in self.times.items()])
-          
+    def __str__(self):
+        return "\n".join([f"{key}: {value}" for key, value in self.times.items()])
