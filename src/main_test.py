5b967a160f375ec54be9b3273bd36a210a53a75d
joris68 <joris.gabrisch@web.de>
2024-05-23 23:33:43 +0200

    improved the test

--- src/main_test.py
+++ src/main_test.py
@@ -3,4 +3,4 @@ from storage_Handler import upload_blob
 
 if __name__ == "__main__":
 
-     upload_blob("experiments-bucket68", 'New/sudden_100.png', 'pic.png')
+     upload_blob("experiments-bucket68", 'New/sudden_100.csv', 'pic.csv')
