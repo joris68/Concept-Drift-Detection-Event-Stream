3684bff9605d1a458fbe3b14f0859a47526f0f90
joris68 <joris.gabrisch@web.de>
2024-05-24 12:07:19 +0200

    added new test

--- src/main_test.py
+++ src/main_test.py
@@ -1,6 +1,17 @@
 from google.cloud import storage
 from storage_Handler import upload_blob
+import csv
 
 if __name__ == "__main__":
 
-     upload_blob("experiments-bucket68", './New/sudden_100.csv', 'pic.csv')
+     try:
+          file = open("./New/sudden_100.csv")
+          writer = csv.writer(file)
+          print("we could open the file successfully")
+     except Exception as e:
+          print(e)
+     finally:
+          file.close()
+          
+
+     #upload_blob("experiments-bucket68", './New/sudden_100.csv', 'pic.csv')
