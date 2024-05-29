942b99abc99a65deb65bd85ffa4c275b2d04ac53
joris68 <joris.gabrisch@web.de>
2024-05-29 17:47:11 +0200

    lets try the traffic dataset, also added incremental dataset

--- Experiments/incremental.py
+++ Experiments/incremental.py
@@ -1,25 +1,128 @@
 import pandas as pd
+import statistics as s
 
-# Load the data
-file_path = 'New/incremental_100.csv'
-df = pd.read_csv(file_path)
+def calc_accuracy():
+    file_path = 'ExperimentsDocker/incremental_100.csv'
+    df = pd.read_csv(file_path)
+    numbers_and_drifts = {}
 
-# Dictionary to store counts
-count_dict = {}
-# Dictionary to store averages for each Trace Threshold and Anomaly Threshold
-average_dict = {}
+    for (experiment, trace_threshold, anomaly_threshold), group in df.groupby(['Experiment','Deviation', 'Cohens Boundary']):
+        # Initialize a dictionary to store counts of each drift type
+        drift_counts = {
+            'DriftType.SUDDEN': 0,
+            'DriftType.RECURRING': 0,
+            'DriftType.INCREMENTAL': 0,
+            'DriftType.SUDDEN_RECURRING': 0,
+            'DriftType.INCREMENTAL_RECURRING': 0,
+            'TOTAL': 0
+        }
 
-# Group by 'Experiment', 'Trace Threshold', and 'Anomaly Threshold' and count entries
-for (experiment, trace_threshold, anomaly_threshold), group in df.groupby(['Experiment', 'Trace Threshold', 'Anomaly Threshold']):
-    # Create a key as a tuple of trace_threshold and anomaly_threshold, accumulate counts
-    key = (trace_threshold, anomaly_threshold)
-    if key not in count_dict:
-        count_dict[key] = []
-    count_dict[key].append(len(group))
+        # Iterate through each row in the group to count drift types
+        for index, row in group.iterrows():
+            drift_type = row["Drifts detected"]
+            if drift_type in drift_counts:
+                drift_counts[drift_type] += 1
 
-# Calculate averages for each Trace Threshold and Anomaly Threshold combination
-for key, counts in count_dict.items():
-    average_dict[key] = sum(counts) / len(counts) if counts else 0
+        # Update the total drifts count
+        drift_counts['TOTAL'] = len(group)
 
-# Print out the average dictionary
-print(average_dict)
+        # Save to the dictionary using (experiment, trace_threshold, anomaly_threshold) as key
+        numbers_and_drifts[(experiment, trace_threshold, anomaly_threshold)] = drift_counts
+
+    return numbers_and_drifts
+
+# I want the number of drifts for different type of deviations, no matter which
+def count_rows_per_group():
+    file_path = 'ExperimentsDocker/incremental_100.csv'
+    df = pd.read_csv(file_path)
+    numbers_and_drifts = {}
+    df_one_experiment = df[df["Experiment"]== 0]
+    for x in [0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2]:
+
+        df_dev = df_one_experiment[df_one_experiment["Deviation"]== x]
+        print(len(df_dev) / 11)
+
+#count_rows_per_group()
+
+def analyze_cohens():
+    file_path = 'ExperimentsDocker/incremental_100.csv'
+    df = pd.read_csv(file_path)
+    df_one_experiment = df[df["Experiment"]== 0]
+    for x in [6, 8, 10, 12, 14, 16, 18, 20]:
+        df_one_experiment
+
+def calc_accuracy_2():
+    file_path = 'ExperimentsDocker/incremental_100.csv'
+    df = pd.read_csv(file_path)
+    numbers_and_drifts = {}
+
+    for (experiment, deviation), group in df.groupby(['Experiment','Deviation']):
+        # Initialize a dictionary to store counts of each drift type
+        drift_counts = {
+            'DriftType.SUDDEN': 0,
+            'DriftType.RECURRING': 0,
+            'DriftType.INCREMENTAL': 0,
+            'DriftType.SUDDEN_RECURRING': 0,
+            'DriftType.INCREMENTAL_RECURRING': 0,
+            'TOTAL': 0
+        }
+
+        # Iterate through each row in the group to count drift types
+        for index, row in group.iterrows():
+            drift_type = row["Drifts detected"]
+            if drift_type in drift_counts:
+                drift_counts[drift_type] += 1
+
+        # Update the total drifts count
+        drift_counts['TOTAL'] = len(group)
+
+        # Save to the dictionary using (experiment, trace_threshold, anomaly_threshold) as key
+        numbers_and_drifts[(experiment, deviation)] = drift_counts
+
+    return numbers_and_drifts
+
+
+
+
+
+def aggregate(accs : dict) -> dict:
+
+    aggregation = {}
+
+    for key, value in accs.items():
+        key_1 = key[1]
+        key_2 = key[2]
+        my_tuple = (key_1, key_2)
+        if my_tuple not in aggregation:
+            aggregation[my_tuple] = drift_counts = {
+            'DriftType.SUDDEN': 0,
+            'DriftType.RECURRING': 0,
+            'DriftType.INCREMENTAL': 0,
+            'DriftType.SUDDEN_RECURRING': 0,
+            'DriftType.INCREMENTAL_RECURRING': 0,
+            'TOTAL': 0
+        }
+        else:
+            aggregation[my_tuple]['DriftType.SUDDEN'] += value['DriftType.SUDDEN']
+            aggregation[my_tuple]['DriftType.RECURRING'] += value['DriftType.RECURRING']
+            aggregation[my_tuple]['DriftType.INCREMENTAL'] += value['DriftType.INCREMENTAL']
+            aggregation[my_tuple]['DriftType.SUDDEN_RECURRING'] += value["DriftType.SUDDEN_RECURRING"]
+            aggregation[my_tuple]['DriftType.INCREMENTAL_RECURRING'] += value['DriftType.INCREMENTAL_RECURRING']
+            aggregation[my_tuple]['TOTAL'] += value['TOTAL']
+    
+    return aggregation
+
+
+
+def auswahl(drifttype, dict_data):
+    to_return = {}
+    for key, value in dict_data.items():
+        if key not in to_return:
+            to_return[key] = value[drifttype]
+    
+    return to_return
+
+#accuracy_results = calc_accuracy()
+#agss = aggregate(accuracy_results)
+#agss_auswahl = auswahl('TOTAL', agss)
+#print(agss_auswahl)
--- src/main_real_traffic.py
+++ src/main_real_traffic.py
@@ -4,20 +4,24 @@ from ProcessHistory import ProcessHistory
 from pybeamline.sources import log_source
 import time
 import csv
-from storage_Handler import upload_blob
+from storage_Handler import upload_blob, download_blob
 
 def offer(allowed_deviation, trace_treshold, lower_boundary, anomaly_treshold, cohens_boundary, model_epsilon, size):
     #logging.basicConfig(filename="logs/logs_sudden.log", encoding="utf-8", level=logging.DEBUG)
     my_trace_map_handler = TraceMapHandler(traceMapSize=size)
     my_model_handler = ModelHandler(my_trace_map_handler, allowed_deviation=allowed_deviation, trace_Treshold=trace_treshold)
     my_process_history = ProcessHistory(my_model_handler, lower_boundary=lower_boundary, anomaly_treshhold=anomaly_treshold, cohens_boundary=cohens_boundary, model_epsilon=model_epsilon)
-    log_source("Data/real-world/Road_Traffic_Fine_Management_Process.xes").pipe().subscribe(lambda x: my_process_history.concept_Drift_detection(x))
+    log_source("DataImported/traffic.xes").pipe().subscribe(lambda x: my_process_history.concept_Drift_detection(x))
     return len(my_process_history.processHistory), my_process_history.driftHistory, my_process_history.historyCohens 
 
 
 
 
 if __name__ == "__main__":
+
+        download_blob("experiments-data-bucket68", "Road_Traffic_Fine_Management_Process.xes", "DataImported/traffic.xes"  )
+
+
 
 
         try:
