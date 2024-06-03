0313d479c6c57ccbffc570fe46e41caf2f1649fb
joris68 <joris.gabrisch@web.de>
2024-06-03 17:51:47 +0200

    last commit

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -16,26 +16,6 @@ provider "google" {
 }
 
 
-#resource "google_cloudbuild_trigger" "github_trigger" {
- ##    provider = google
-  #   name = "github-trigger"
- #    description = "Trigger for GitHub repo changes"
-
- # github {
- #   owner = "joris68"
- #   name  = "Concept-Drift-Detection-Event-Stream"
-#    push {
-#      branch = "^experiments$"  // Trigger on changes to the main branch
-#    }
-#  }
-
-##  filename = "cloudbuild.yaml"  // The name of your Cloud Build config file
-#}
-
-
-
-
-
 # Docker repo for the images
 resource "google_artifact_registry_repository" "my-repo" {
   location      = var.region
--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -1,14 +1,11 @@
 variable "project_id" {
      type = string
-  default = "bachelorthesis-421507"
 }
 
 variable "region" {
   type = string
-  default = "europe-west1"
 }
 
 variable "docker_repo_name" {
   type = string
-  default = "mythesisimages"
 }
